f708c9df597b157dd781a4921699d1ce2d7497ef
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-05-28 16:49:31 +0530

    Update: Server code for deploy

--- server/server.js
+++ server/server.js
@@ -14,5 +14,7 @@ app.use(express.json({ extended: false }));
 
 app.use('/api', authRoutes);
 
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the MERN Authentication API' }));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
